57c09b8eb5796a73c618ca9c30177c1a42f55277
istRazvan9 <125460811+istRazvan9@users.noreply.github.com>
2023-02-23 16:48:35 +0200

    3021- Add new entity model

--- ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/data_classes/AccessCode.kt
+++ ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/data_classes/AccessCode.kt
@@ -3,19 +3,26 @@ package com.levi9.ppac.service.api.data_classes
 import com.levi9.ppac.service.api.domain.AccessCodeEntity
 import com.levi9.ppac.service.api.enums.CodeType
 import java.util.*
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 class AccessCode(
-    val id: UUID = UUID.randomUUID()
+    @NotNull
+    val id: UUID,
+    @NotNull
+    @Min(value = 100000, message = "Value should have minimum 6 characters.")
+    @Max(value = 999999, message = "Value should have maximum 6 characters.")
+    var value: Int
 ) {
-    lateinit var value: String
 
-    var type: CodeType? = null
-
-    var companyId: UUID? = null
+    @Pattern(regexp = "^(ADMIN_CODE|COMPANY_CODE)$", message = "CodeType should have value ADMIN_CODE or COMPANY_CODE.")
+    var type: CodeType? = CodeType.COMPANY_CODE
 
     companion object {
         fun parse(elem: AccessCodeEntity): AccessCode {
-            return AccessCode(elem.id).apply {
+            return AccessCode(elem.id, 0).apply {
                 value = elem.value
                 type = elem.type
                 companyId = elem.companyId
@@ -23,7 +30,7 @@ class AccessCode(
         }
 
         fun parse(elem: AccessCode): AccessCodeEntity {
-            return AccessCodeEntity(elem.id).apply {
+            return AccessCodeEntity(elem.id, 0).apply {
                 value = elem.value
                 type = elem.type
                 companyId = elem.companyId
