f013cc86e6009b611f8a248ebf674d578a13b7e0
istRazvan9 <125460811+istRazvan9@users.noreply.github.com>
2023-02-24 12:55:54 +0200

    3021 - Add Foreign Key to CompanyEntity model

--- ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/data_classes/CompanyCode.kt
+++ ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/data_classes/CompanyCode.kt
@@ -4,14 +4,13 @@ import com.levi9.ppac.service.api.domain.CompanyCodeEntity
 import java.util.*
 
 class CompanyCode (
-    var code: UUID )
+    var code: UUID, var id: UUID )
 {
     lateinit var companyId : UUID
 
     companion object {
         fun parse(elem: CompanyCodeEntity): CompanyCode {
-            return CompanyCode(elem.code).apply {
-                companyId = elem.companyId
+            return CompanyCode(elem.id, elem.codeId.id).apply {
             }
         }
     }
