ef6ddb8f4a2a2b64665a07a78fa604816a48d45b
istRazvan9 <125460811+istRazvan9@users.noreply.github.com>
2023-02-24 12:55:54 +0200

    3021 - Add Foreign Key to CompanyEntity model

--- ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/domain/CompanyCodeEntity.kt
+++ ppac-service-common/src/main/kotlin/com/levi9/ppac/service/api/domain/CompanyCodeEntity.kt
@@ -6,21 +6,24 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
 import javax.persistence.Table
 
 @Entity
-@Table(name ="COMPANIES_CODES")
-data class CompanyCodeEntity (
+@Table(name = "COMPANIES_CODES")
+data class CompanyCodeEntity(
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "CODE_ID")
-    var code: UUID
+    @Column(name = "ID")
+    var id: UUID,
+    @OneToOne
+    @JoinColumn(name = "CODE_ID", referencedColumnName = "ID")
+    val codeId: AccessCodeEntity,
 
-){
+    @OneToOne
+    @JoinColumn(name = "COMPANY_ID", referencedColumnName = "ID")
+    val companyId: CompanyEntity
 
-    @Column(name = "COMPANY_ID", nullable = false)
-    lateinit var companyId : UUID
-
-
-}
+)
