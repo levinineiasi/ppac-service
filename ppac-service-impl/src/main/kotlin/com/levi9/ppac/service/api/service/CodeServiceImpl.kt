722ee3094f3df17c6e4f0da57bb524e8b381a24e
Madalina Carausu <madalinacarausu@gmail.com>
2023-02-28 16:10:37 +0200

    3021 - Add logging messages

--- ppac-service-impl/src/main/kotlin/com/levi9/ppac/service/api/service/CodeServiceImpl.kt
+++ ppac-service-impl/src/main/kotlin/com/levi9/ppac/service/api/service/CodeServiceImpl.kt
@@ -9,14 +9,18 @@ import com.levi9.ppac.service.api.repository.CompanyRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.logging.Logger
 import javax.transaction.Transactional
 import kotlin.random.Random
 
 @Service
 class CodeServiceImpl(
-    val codeRepository: CodeRepository,
-    val companyRepository: CompanyRepository,
-    val companyCodeRepository: CompanyCodeRepository
+        val codeRepository: CodeRepository,
+        val companyRepository: CompanyRepository,
+        val companyCodeRepository: CompanyCodeRepository,
+
+        val log: Logger = Logger.getLogger(CodeServiceImpl::class.java.name)
+
 ) : CodeService<CompanyCode> {
     @Transactional
     override fun findAll(): List<CompanyCode> {
@@ -35,15 +39,23 @@ class CodeServiceImpl(
             value = Random.nextInt(100000, 999999);
         }
 
+        log.info("Generated $value value for $displayName company.")
+
         val accessCodeDTO = AccessCode(UUID.randomUUID(), value)
         val accessCodeEntity = codeRepository.save(AccessCode.parse(accessCodeDTO))
+
+        log.info("Inserted accessCodeEntity with id ${accessCodeEntity.id} into database.")
 
         val companyDTO = Company(UUID.randomUUID(), displayName)
         val companyEntity = companyRepository.save(Company.parse(companyDTO))
 
+        log.info("Inserted companyEntity with id ${companyEntity.id} into database.")
+
         val companyCodeDTO =
-            CompanyCode(UUID.randomUUID(), AccessCode.parse(accessCodeEntity), Company.parse(companyEntity));
+                CompanyCode(UUID.randomUUID(), AccessCode.parse(accessCodeEntity), Company.parse(companyEntity));
         val companyCodeEntity = companyCodeRepository.save(CompanyCode.parse(companyCodeDTO))
+
+        log.info("Inserted companyCodeEntity with id ${companyCodeEntity.id} into database.")
 
         return CompanyCode.parse(companyCodeEntity)
     }
