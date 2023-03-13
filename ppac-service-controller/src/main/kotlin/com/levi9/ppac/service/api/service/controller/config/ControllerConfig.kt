cdfaff036ec50f94262f3bdf1ca0191bc7cead3a
Stefan Stan <19stephan93@gmail.com>
2023-03-13 15:12:00 +0200

    fixed CORS setting

--- ppac-service-controller/src/main/kotlin/com/levi9/ppac/service/api/service/controller/config/ControllerConfig.kt
+++ ppac-service-controller/src/main/kotlin/com/levi9/ppac/service/api/service/controller/config/ControllerConfig.kt
@@ -1,20 +1,17 @@
 package com.levi9.ppac.service.api.service.controller.config
 
+import com.levi9.ppac.service.api.logging.logger
 import com.levi9.ppac.service.api.service.controller.config.interceptors.HttpControllerInterceptor
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
-import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.GroupedOpenApi
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.handler.MappedInterceptor
@@ -22,13 +19,9 @@ import org.springframework.web.servlet.handler.MappedInterceptor
 
 @Configuration
 internal class ControllerConfig {
-
     companion object {
         private const val responseTime = 1000
     }
-
-    @Value("\${server.allowed-origins}")
-    private val allowedOrigins: String? = null
 
     @Bean
     fun openApiConfig(): OpenAPI? {
@@ -59,22 +52,12 @@ internal class ControllerConfig {
         return GroupedOpenApi.builder().group("v1").pathsToMatch(*paths).packagesToScan(*packages).build()
     }
 
-    @Bean
-    fun corsFilter(): CorsFilter? {
-        val source = UrlBasedCorsConfigurationSource()
-
-        // Allow anyone and anything access. Probably ok for Swagger spec
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.addAllowedOrigin(allowedOrigins)
-        config.addAllowedHeader("*")
-        config.addAllowedMethod("*")
-        source.registerCorsConfiguration("/api/v1/", config)
-        return CorsFilter(source)
-    }
-
     @Component
-    internal class ControllerInterceptorConfig : WebMvcConfigurer {
+    internal class ControllerInterceptorConfig(
+        @Value("\${server.allowed-origins}")
+        private val allowedOrigins: String? = null
+    ): WebMvcConfigurer {
+
         override fun addInterceptors(registry: InterceptorRegistry) {
             registry.addInterceptor(
                 MappedInterceptor(
@@ -83,6 +66,14 @@ internal class ControllerConfig {
                     HttpControllerInterceptor()
                 )
             )
+        }
+
+        override fun addCorsMappings(registry: CorsRegistry) {
+            logger.debug("allowedOrigins: $allowedOrigins")
+            registry.addMapping("/api/v1/**")
+                    .allowedOrigins(allowedOrigins)
+                    .allowedHeaders("*")
+                    .allowedMethods("*")
         }
     }
 }
